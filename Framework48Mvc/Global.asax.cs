d6693035a7f24ce7ff4407b79803e6fe66050550
lemonwasp <htg2001gd141@gmail.com>
2026-08-04 00:12:16 +0900

    feat: add application logging with log4net

--- Framework48Mvc/Global.asax.cs
+++ Framework48Mvc/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Unity.Mvc5;
+using log4net.Config;
+using System.IO;
 
 namespace Framework48Mvc
 {
@@ -13,6 +15,7 @@ namespace Framework48Mvc
     {
         protected void Application_Start()
         {
+            XmlConfigurator.Configure(new FileInfo(Server.MapPath("~/log4net.config")));
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
